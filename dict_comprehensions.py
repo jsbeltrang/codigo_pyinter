510e36d2682a73264bcf8380bd50b9b3ac1315c2
Sebastian Beltran <js.beltran13@hotmail.com>
2022-09-19 08:37:17 -0500

    Se realizó la actividad de dictionaries comprehensions

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -1,3 +1,4 @@
+import  math
 def run():
     # my_dict = {}
 
@@ -5,7 +6,11 @@ def run():
     #     if i % 3 != 0:
     #         my_dict[i] = i**3
 
-    my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    # my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+
+    # print(my_dict)
+
+    my_dict = {i: math.sqrt(i) for i in range(1, 1001)}
 
     print(my_dict)
 

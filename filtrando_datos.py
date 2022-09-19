ea7977f2383c08e5f4b03d6c3522a70919e2994a
Sebastian Beltran <js.beltran13@hotmail.com>
2022-09-19 18:07:30 -0500

    Se reaizo  el ejercicio de filtrado de datos

--- filtrando_datos.py
+++ filtrando_datos.py
@@ -72,14 +72,26 @@ DATA = [
 ]
 
 def run():
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    # all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
     
-    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    # all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
 
-    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
-    adults = list(map(lambda worker: worker ["name"], adults))
 
-    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    # adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    # adults = list(map(lambda worker: worker ["name"], adults))
+
+    # old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+
+    all_python_devs = list(filter(lambda worker : worker["language"] == "python", DATA))
+    all_python_devs = list(map(lambda worker : worker ["name"], all_python_devs))
+
+    all_Platzi_workers = list(filter(lambda worker : worker ["organization"] == "Platzi", DATA))
+    all_Platzi_workers = list(map(lambda worker : worker ["name"], all_Platzi_workers))
+
+
+    adults = [worker["name"] for worker in DATA if worker["age"] > 18]
+
+    old_people = [worker["name"] for worker in DATA if worker["age"] > 70]
 
     for worker in old_people:
         print(worker)
